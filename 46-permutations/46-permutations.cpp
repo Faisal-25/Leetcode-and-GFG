52c6ebae495cd5fae7921de7621267146e7130d9
Faisal-25 <95731066+Faisal-25@users.noreply.github.com>
2022-04-27 02:04:25 +0530

    Time: 12 ms (9.33%), Space: 8.6 MB (11.74%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void permutations(vector<int>&nums,vector<bool> &check,vector<int> v,int ci){
+        //bool array of size nums.size() and apply recursive call
+        if(ci == nums.size()){
+            ans.push_back(v);
+            return;
+        }
+        for(int i = 0;i<nums.size();i++){
+            if(!check[i]){
+                check[i]=true;
+                v.push_back(nums[i]);
+                permutations(nums,check,v,ci+1);
+                v.pop_back();
+                check[i] = false;
+            }
+            
+            
+        }
+        
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<bool> check(nums.size());
+        vector<int> v;
+        permutations(nums,check,v,0);
+        return ans;
+    }
+};
