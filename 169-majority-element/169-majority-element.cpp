f00b8c6e11cdf646c3873fbd815e67f83ed6d124
Faisal-25 <95731066+Faisal-25@users.noreply.github.com>
2022-06-03 16:04:20 +0530

    Time: 36 ms (19.50%), Space: 19.5 MB (92.44%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+      bool isValid(vector<int>&a,int n,int val){
+        int freq = 0;
+        for(int i = 0;i<n;i++){
+            int num = a[i];
+             freq+=num == val?1:0;
+        }
+        return freq>n/2;
+    }
+    int majorityElement(vector<int>& a,int n = 0) {
+        n = a.size();
+        int count = 1;
+        int val =a[0];
+        for(int i = 0;i<n;i++){
+            if(a[i] == val)count++;
+            else count--;
+            if(count == 0){
+                val = a[i];
+                count = 1;
+            }
+        }
+       return isValid(a,n,val)?val:-1;
+        
+    }
+};
