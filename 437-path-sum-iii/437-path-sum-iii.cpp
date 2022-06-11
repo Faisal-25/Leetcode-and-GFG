cba119499811b013e41a8f6800054c9c60f47b2d
Faisal-25 <95731066+Faisal-25@users.noreply.github.com>
2022-06-11 15:01:09 +0530

    Time: 31 ms (57.38%), Space: 20.9 MB (25.17%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -10,41 +10,34 @@
  * };
  */
 class Solution {
+public:
+    
+    unordered_map<int,int> m;
     int count = 0;
-    private:
-    void PS_2(TreeNode*root, int targetSum, long long int sum ,vector<int> s){
-        if(root == NULL)return;
+    void pathSumIII(TreeNode*root,long long int sum,int targetSum){
+        if(root == nullptr)return;
         sum+=root->val;
-        s.push_back(root->val);
-        if(sum == targetSum){
-            count++;
-            int dum = sum;
-            for(int i = 0;i<s.size()-1;i++){
-                dum = dum-s[i];
-                if(dum == targetSum){
-                    count++;
-                }
-            }
+          if(m[sum-targetSum]>0){
+            count+=m[sum-targetSum];
         }
-        else {
-            long long int  dum = sum;
-            for(int i = 0;i<s.size()-1;i++){
-                dum = dum - s[i];
-                if(dum == targetSum){
-                    count++;
-                }
-            }
-        }
-     
-        PS_2(root->left, targetSum,sum,s);
-        PS_2(root->right, targetSum,sum,s);
+        m[sum]++;
+      
+        pathSumIII(root->left, sum,targetSum);
+        pathSumIII(root->right,sum,targetSum);
+    
+        m[sum]--;
+        
+        
     }
-    public:
     int pathSum(TreeNode* root, int targetSum) {
-        if(root == NULL)return 0;
-        vector<int> s;
-        long long int sum = 0;
-        PS_2(root, targetSum,sum,s);
+        if(root == nullptr)return 0;
+        if(root->left == NULL and root->right == NULL){
+            if(root->val!=targetSum)return 0;
+        }
+        unordered_map<int,int> t;
+        m = t;
+        m[0]++;
+        pathSumIII(root,0,targetSum);
         return count;
     }
 };
