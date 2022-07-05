2679a798b5ca31eb8af437a01c0b4daf7f633745
Faisal-25 <95731066+Faisal-25@users.noreply.github.com>
2022-07-05 13:35:33 +0530

    Time: 265 ms (65.59%), Space: 67.4 MB (52.15%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -9,11 +9,9 @@ public:
         //     if(count[v[1]] == edges.size())return v[1];
         // }
        
-        if(edges[0][0] == edges[1][0])return edges[0][0];
-        else if(edges[0][0] == edges[1][1])return edges[0][0];
-      else if(edges[0][1] == edges[1][0])return edges[0][1];
-        else if(edges[0][1] == edges[1][1])return edges[0][1];
-        
+        if(edges[0][0] != edges[1][0] && edges[0][0]!=edges[1][1])return edges[0][1];
+        return edges[0][0];
+      
      return 0;
     }
 };
