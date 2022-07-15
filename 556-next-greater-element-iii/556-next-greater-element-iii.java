3507b3c0d549dce4dc6f289758ad4335247a2a83
Faisal-25 <95731066+Faisal-25@users.noreply.github.com>
2022-07-16 00:12:55 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (20.90%) - LeetHub

---
+++ 556-next-greater-element-iii/556-next-greater-element-iii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int nextGreaterElement(int n) {
+        
+        int[] freq = new int[10];
+        
+        long temp = n;
+        int prev = 0;
+        while(prev <= temp%10){
+            prev = (int) temp%10;
+            freq[prev]++;
+            temp = temp/10;
+        }
+        if(temp == 0 ){
+            return -1;
+        }
+        int toReplace = (int) temp%10;
+        temp = temp/10;
+        freq[toReplace]++;
+        
+        for(int i=toReplace+1; i<10; i++){
+            if(freq[i]>0){
+                temp = temp*10 + i;
+                freq[i]--;
+                break;
+            }
+        }
+        for(int i=0;i<freq.length; i++){
+            while(freq[i]>0){
+                temp = temp*10 + i;
+                freq[i]--;
+            }
+        }
+        
+        if(temp>Integer.MAX_VALUE){
+            return -1;
+        }
+        
+       
+        
+        return (int)temp;
+    }
+}
