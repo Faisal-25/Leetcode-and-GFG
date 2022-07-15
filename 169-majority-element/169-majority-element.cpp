79302665001a4a3c496683936fc83dd364277c92
Faisal-25 <95731066+Faisal-25@users.noreply.github.com>
2022-07-15 23:57:37 +0530

    Time: 22 ms (82.68%), Space: 19.6 MB (58.28%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,33 +1,7 @@
 class Solution {
 public:
-      bool isValid(vector<int>&a,int n,int val){
-        int freq = 0;
-        for(int i = 0;i<n;i++){
-            int num = a[i];
-             freq+=num == val?1:0;
-        }
-        return freq>n/2;
-    }
-    int majorityElement(vector<int>& a,int n = 0) {
-       //  n = a.size();
-       //  int count = 1;
-       //  int val =a[0];
-       //  for(int i = 0;i<n;i++){
-       //      if(a[i] == val)count++;
-       //      else count--;
-       //      if(count == 0){
-       //          val = a[i];
-       //          count = 1;
-       //      }
-       //  }
-       // return isValid(a,n,val)?val:-1;
-        unordered_map<int,int> m;
-        for(int val:a){
-            m[val]++;
-        }
-        for(auto&[k,v]:m){
-            if(m[k]>a.size()/2)return k;
-        }
-        return -1;
+    int majorityElement(vector<int>& nums) {
+        nth_element(nums.begin(), nums.begin() + nums.size() / 2, nums.end());
+        return nums[nums.size() / 2];
     }
 };
