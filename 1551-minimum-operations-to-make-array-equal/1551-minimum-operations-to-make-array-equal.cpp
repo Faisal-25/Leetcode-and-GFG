2396df8e0a3f73e288dcede90f18dde70b43ca01
Faisal-25 <95731066+Faisal-25@users.noreply.github.com>
2022-07-23 22:58:48 +0530

    Time: 2 ms (59.03%), Space: 5.9 MB (33.68%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int minOperations(int n) {
-        int sum = 0;
-        vector<int> v;
-        for(int i = 0;i<n;i++){
-            v.push_back(2*i+1);
-            sum+=(2*i+1);
+        int avg = (n*n)/n;
+        int count = 0;
+        
+        for(int i = 0;i<n/2;i++){
+            count+=abs(avg-(2*i+1));
         }
-        int avg = sum/n;
-        int i = 0, j = n-1,count = 0;
-        while(i<j){
-            count+=abs(avg-v[i]);
-            i++;
-            j--;
-        }
+      
         return count;
         
     }
