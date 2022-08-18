e4ccade7f6b8baa2c0f31d543d3ca031a7da2b79
Faisal-25 <95731066+Faisal-25@users.noreply.github.com>
2022-08-18 23:50:18 +0530

    Time: 213 ms (92.34%), Space: 78.3 MB (79.94%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int minSetSize(vector<int>& arr) {
         unordered_map<int,int> m;
-        int n = arr.size();
-        for(int i = 0;i<n;i++){
+        int N = arr.size();
+        for(int i = 0;i<N;i++){
             m[arr[i]]++;
         }
-
-        vector<pair<int,int>> ans;
-        for(auto &[k,v]:m){
-            ans.push_back(make_pair(k,v));
+        vector<int> s;
+        for(auto&[k,v]:m){
+            s.push_back(v);
         }
-        sort(ans.begin(),ans.end(),[](const pair<int,int>&p1,const pair<int,int>&p2){
-            return p1.second>p2.second;
+        sort(s.begin(),s.end(),[](const int &v1,const int &v2){
+            return v1>v2;
         });
-        int set = 0;
         int count = 0;
-        for(int i = 0;i<ans.size();i++){
-            count+=ans[i].second;
+        int set = 0;
+        for(int num:s){
+            count+=num;
             set++;
-            if(count>=n/2)return set;
+            if(count>=N/2)return set;
         }
         return -1;
     }
+    
 };
