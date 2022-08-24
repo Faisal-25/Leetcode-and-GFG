1fd16067e5fd49b278a961f56b5a78102cb4f688
Faisal-25 <95731066+Faisal-25@users.noreply.github.com>
2022-08-25 02:32:54 +0530

    Time: 16 ms (94.46%), Space: 5.8 MB (88.51%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n <=0)return false;
-         if(n == 1)return true;
-     
-        while(n>1){
-            cout<<n<<endl;
-            if(n%3 !=0)return false;
-            n = n/3;
-        }
-       
-        return true;
+        if(n<=0)return false;
+        if(n == 1)return true;
+        if(n%3!=0)return false;
+        return isPowerOfThree(n/3);
     }
 };
