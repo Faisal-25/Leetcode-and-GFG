cd03c3c47bb60495a263ecd683129076a77e9e8d
Faisal-25 <95731066+Faisal-25@users.noreply.github.com>
2022-09-17 21:50:35 +0530

    Time: 8 ms (12.42%), Space: 8.4 MB (34.28%) - LeetHub

---
+++ 655-print-binary-tree/655-print-binary-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode*root){
+        if(root == nullptr)return 0;
+        return max(height(root->left), height(root->right))+1;
+    }
+     void solve(TreeNode*root, int l, int r,int h,vector<vector<string>>&ans){
+        if(root == nullptr)return;
+        int mid = (l+r)/2;
+        ans[h][mid] = to_string(root->val);
+        solve(root->left, l,mid-1,h+1,ans); 
+        solve(root->right,mid+1,r, h+1,ans);
+    }
+    vector<vector<string>> printTree(TreeNode* root) {
+        int h = height(root);
+        int c = pow(2,h) -1;
+        vector<vector<string>> ans(h,vector<string>(c));
+        solve(root,0,c-1,0,ans);
+        return ans;
+    }
+   
+};
