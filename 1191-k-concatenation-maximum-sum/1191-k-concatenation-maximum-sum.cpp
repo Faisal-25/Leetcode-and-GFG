855f7e7a79f158f1a9da846ca0a1e7f30bcaea95
Faisal-25 <95731066+Faisal-25@users.noreply.github.com>
2022-09-21 16:16:01 +0530

    Time: 211 ms (5.18%), Space: 42.4 MB (35.82%) - LeetHub

--- 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.cpp
+++ 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.cpp
@@ -1,27 +1,50 @@
 class Solution {
-public:
-   int kConcatenationMaxSum(vector<int>& arr, int k) {
-	long long ans=0,curr=0,last;
-	// Apply kadane's algo by traversing over the array two times to know the pattern
-	// If at the end of second traversal, if sum (curr variable is storing the sum) is greater than what we get from 1st travresal then this means that (k-2) arrays when concatenated will always increase our sum
-	// else our answer will be from first 2 arrays or traversal
-	for(int i=1;i<=k && i<=2;i++){
-		last=curr;  // storing sum of 1st traversal
-		for(auto ele:arr){
-			curr=curr+ele;
-			if(curr<ele){
-				curr=ele;
-			}
-			ans=(max(ans,curr))%1000000007;
-			curr%=1000000007;
-		}
-	}
-	// let the sums we got after 1st and 2nd traversal are 5 and 10 respectively.
-	// Here, there is increase of 5 in sum(10-5=5). So when the remaining k-2 arrays are concatenated, the increase in sum will (k-2)*5
-	if(curr-last>0 && k>1){
-		ans=(ans+((curr-last)*(k-2))%1000000007)%1000000007;
-	}
-	return ans;
-}
-// Hit Upvote if you like... :)
+    public:
+     int mod = pow(10,9)+7;
+     int kadane(vector<int> &arr){
+        int curSum = 0;
+        int maxSum = INT_MIN;
+        for(int i = 0;i<arr.size();i++){
+            curSum+=arr[i];
+            if(maxSum<curSum){
+                maxSum = curSum;
+            }
+            if(curSum<0){
+                curSum = 0;
+            }
+        }
+        
+     
+        return maxSum%mod;
+        
+    }
+     int kConcatenationMaxSum(vector<int> &arr, int k) {
+        int sum = 0;
+        for(int i = 0;i<arr.size();i++){
+            sum+=arr[i];
+        }
+        // System.out.println(sum);
+        vector<int> newArr(2*arr.size());
+        for(int i = 0;i<2*arr.size();i++){
+            if(i<arr.size()){
+                newArr[i] = arr[i];
+            }
+            else{
+                newArr[i] = arr[(i-arr.size())];
+            }
+            
+        }
+         if(k == 1){
+            return kadane(arr);
+        }
+  
+        int ans = kadane(newArr);
+        if(sum<0){
+            
+            return ans>0?ans:0;
+        }
+        return (ans + (long long int)((long long int)(k-2)*sum))%mod;
+       
+        
+    }
 };
